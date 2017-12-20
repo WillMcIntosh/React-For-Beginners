9491d15bab74d00423a414372049aa26fcc15fa3
WillMcIntosh <mcintoshwilliamh@gmail.com>
2017-12-19 21:21:58 -0500

    helper and utility functions and setting up react events to change url

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,6 +1,29 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    // need to bind this to StorePicker in goToStore
+    // could use a constructor
+    // constructor() {
+    // super();
+    // this.goToStore = this.goToStore.bind(this);
+    // }
+    // OR
+    // do it in the onSubmit below
+    // as this.goToStore.bind(this)
+    // OR with an arrow function
+    // (e) => this.goToStore(e)
+    
+    goToStore(event) {
+        // stop React from refreshing page
+        event.preventDefault();
+        console.log('You changed the URL');
+        // first grab text from box
+        console.log(this.storeInput.value);
+        
+        // second transition from / to /store/storeid
+    }
+    // render is always bound to the Component so this refers to Component
     render() {
         // return <p>Hello.</p>;
         // can return single elements like so, or
@@ -8,10 +31,11 @@ class StorePicker extends React.Component {
         // OR:
             // jsx can only return one parent element
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
             {/* JSX comment */}
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" />
+                <input type="text" required placeholder="Store Name"
+                    defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
                 <button type="submit">Visit Store</button>
             </form>
 
