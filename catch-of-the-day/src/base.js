47f1b0631a70bd04e35f79121700c2276699276f
WillMcIntosh <mcintoshwilliamh@gmail.com>
2018-01-27 23:59:29 -0500

    added firebase support and updating state fixed error with storeId

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyAK7rLnsduj0XJ1MTPpdmpAdsmQsEyV138",
+  authDomain: "catch-of-the-day-will-mcintosh.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-will-mcintosh.firebaseio.com",
+});
+
+export default base;
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,7 @@ import Order from './Order';
 import Inventory from './Inventory';
 import SampleFishes from '../sample-fishes';
 import Fish from './Fish';
+import base from '../base';
 
 class App extends React.Component {
   constructor() {
@@ -15,11 +16,23 @@ class App extends React.Component {
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
+
     // getInitialState
     this.state = {
       fishes: {},
       order: {}
     };
+  }
+
+  componentWillMount() {
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes`, {
+      context: this,
+      state: 'fishes'
+    });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
   }
 
   addFish(fish) {
